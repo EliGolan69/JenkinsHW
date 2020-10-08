051e2660b59380847c718ae5ba97af87724f9972
Eli Golan <69590001+EliGolan69@users.noreply.github.com>
2020-10-08 09:41:29 +0300

    Delete MoveFile.py

--- MoveFile.py
+++
@@ -1,7 +0,0 @@
-import shutil
-import os
-
-from_file_name = os.environ["HOMEPATH"] + "\Desktop\MyName.txt"
-to_file_name = "c:\Python\MyName.txt"
-if os.path.exists(from_file_name):
-   shutil.move(from_file_name, to_file_name)
